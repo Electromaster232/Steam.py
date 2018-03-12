a25b6c96bb0c8e95e78d6a27b5d6ce9424fc97d8
Electromaster232 <gilbert7573@gmail.com>
2018-03-11 21:50:28 -0400

    ADDED HOLY FUCK TON OF STUFF

---
+++ cogs/utils.py
@@ -0,0 +1,30 @@
+import requests
+
+
+class Utils:
+
+    def vanitytosteamid(vanityurl):
+        """Convert a vanity url to SteamID64"""
+
+        response = requests.get(
+            "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=FF0EEF99E5BD63F29FC0F938A56F115C&vanityurl=" + vanityurl)
+        resjson = response.json()
+        if resjson['response']['success'] != 1:
+            return False
+        return resjson['response']['steamid']
+
+    def gametoid(gamename):
+        """Convert a game name to its ID"""
+
+        response = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v2")
+        response = response.json()
+        response = response['applist']['apps']
+        try:
+            gameid = next((item for item in response if item["name"] == gamename))
+        except StopIteration:
+            return False
+        gameid = gameid['appid']
+        return gameid
+
+    def setup(self):
+        pass
--- main.py
+++ main.py
@@ -2,17 +2,22 @@ from discord.ext import commands
 import discord
 import logging
 import asyncio
+import traceback
+import sys
 
 description = '''Steam.py Alpha v0.1'''
 # Set logging level
 logging.basicConfig(level=logging.ERROR)
 
 # this specifies what extensions to load when the bot starts up
-startup_extensions = []
+startup_extensions = ['cogs.steammain', 'cogs.gameinfo']
 
 # Create the bot object
 bot = commands.Bot(command_prefix=['steam.'], description=description)
 
+
+def ownercheck(ctx):
+    return ctx.message.author.id == "133353440385433600"
 
 @bot.event
 async def on_ready():
@@ -21,8 +26,11 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('READY')
+    await bot.change_presence(game=discord.Game(name="{} Users and {} Guilds!".format(
+        len(set(bot.get_all_members())), len(bot.servers)), type=3))
 
 
+@commands.check(ownercheck)
 @bot.command()
 async def load(extension_name: str):
     """Loads an extension."""
@@ -33,7 +41,7 @@ async def load(extension_name: str):
         return
     await bot.say("The extension {} was loaded.".format(extension_name))
 
-
+@commands.check(ownercheck)
 @bot.command()
 async def unload(extension_name: str):
     """Unloads an extension."""
@@ -43,13 +51,22 @@ async def unload(extension_name: str):
         await bot.say("There was an error")
     await bot.say("The extension {} was unloaded.".format(extension_name))
 
-
+@commands.check(ownercheck)
 @bot.command()
 async def reload(extension_name: str):
     """Reloads an extension"""
-    bot.unload_extension("cogs.{}".format(extension_name))
-    bot.load_extension("cogs.{}".format(extension_name))
-    bot.say("The extension {} was reloaded.".format(extension_name))
+    try:
+        bot.unload_extension("cogs.{}".format(extension_name))
+    except (AttributeError, ImportError) as error:
+        await bot.say("```py\n{}: {}\n```".format(type(error).__name__, str(error)))
+        return
+    await bot.say("The extension {} was unloaded.".format(extension_name))
+    try:
+        bot.load_extension("cogs.{}".format(extension_name))
+    except (AttributeError, ImportError) as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await bot.say("The extension {} was loaded.".format(extension_name))
 
 
 @bot.command()
@@ -67,30 +84,80 @@ async def owner():
     await bot.say(embed=em)
 
 
-# TODO: Make this less hacky
+@commands.check(ownercheck)
 @bot.command(pass_context=True)
 async def debug(ctx, *, code):
+    """Evaluate Code"""
 
-    if ctx.message.author.id == "133353440385433600":
-        global_vars = globals().copy()
-        global_vars['bot'] = bot
-        global_vars['ctx'] = ctx
-        global_vars['message'] = ctx.message
-        global_vars['author'] = ctx.message.author
-        global_vars['channel'] = ctx.message.channel
-        global_vars['server'] = ctx.message.server
+    global_vars = globals().copy()
+    global_vars['bot'] = bot
+    global_vars['ctx'] = ctx
+    global_vars['message'] = ctx.message
+    global_vars['author'] = ctx.message.author
+    global_vars['channel'] = ctx.message.channel
+    global_vars['server'] = ctx.message.server
 
-        try:
-            result = eval(code, global_vars, locals())
-            if asyncio.iscoroutine(result):
-                result = await result
-            result = str(result)
-            await bot.say("```" + result + "```")
-        except Exception as error:
-            await bot.say('```{}: {}```'.format(type(error).__name__, str(error)))
-            return
+    try:
+        result = eval(code, global_vars, locals())
+        if asyncio.iscoroutine(result):
+            result = await result
+        result = str(result)
+        await bot.say("```" + result + "```")
+    except Exception as error:
+        await bot.say('```{}: {}```'.format(type(error).__name__, str(error)))
+        return
+
+
+@bot.command()
+async def info():
+    """Bot Information"""
+    await bot.change_presence(game=discord.Game(name="{} Users and {} Guilds!".format(
+        len(set(bot.get_all_members())), len(bot.servers)), type=3))
+    embed = discord.Embed(title="Bot Info", url="https://gitlab.adamegilbert.me/public/",
+                          description="Steam.py bot info", color=0x88ddec)
+    embed.set_thumbnail(
+        url="https://cdn.discordapp.com/avatars/133353440385433600/a_5011d8aa9663db347bfc540a955a10f6.gif")
+    embed.add_field(name="Bot Author", value="DJ Electro#8890", inline=False)
+    embed.add_field(name="Bot Info",
+                    value="Steam.py is a multifunction Steam API bot, designed to house complete access to the multiple Steam APIs, allowing you to not have to have multiple Steam bots.",
+                    inline=True)
+    embed.set_footer(text="Project Started on 3/10/18")
+    await bot.say(embed=embed)
+
+@bot.event
+async def on_command_error(event, ctx):
+    if isinstance(event, commands.CheckFailure):
+        await bot.send_message(ctx.message.channel, ":no_entry: Access to this command is restricted.")
+    if isinstance(event, commands.MissingRequiredArgument):
+        await send_cmd_help(ctx)
+    if isinstance(event, commands.CommandNotFound):
+        pass
     else:
-        await bot.say(":no_entry: Access to this command is restricted.")
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(event), event, event.__traceback__, file=sys.stderr)
+
+
+async def send_cmd_help(ctx):
+    if ctx.invoked_subcommand:
+        pages = bot.formatter.format_help_for(ctx, ctx.invoked_subcommand)
+        for page in pages:
+            await bot.send_message(ctx.message.channel, page)
+    else:
+        pages = bot.formatter.format_help_for(ctx, ctx.command)
+        for page in pages:
+            await bot.send_message(ctx.message.channel, page)
+
+
+@bot.event
+async def on_command(command, ctx):
+    embed = discord.Embed(title="Bot command!", description="A command has been ran!", color=0x88ddec)
+    embed.set_thumbnail(url=ctx.message.author.avatar_url)
+    embed.add_field(name="Command Author", value=str(ctx.message.author), inline=True)
+    embed.add_field(name="Command Ran", value=str(ctx.message.content), inline=True)
+    embed.add_field(name="Server", value=str(ctx.message.server.name), inline=True)
+    embed.add_field(name="Author ID", value=str(ctx.message.author.id), inline=True)
+    embed.set_footer(text="Steam.py logging system")
+    await bot.send_message(bot.get_channel('422570683600732171'), embed=embed)
 
 # Load extensions and start the bot
 if __name__ == "__main__":
@@ -101,4 +168,4 @@ if __name__ == "__main__":
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Failed to load extension {}\n{}'.format(extension, exc))
 # Run the bot object with token
-    bot.run('')
+    bot.run('NDIxNDgwMzM3NDk1OTQ5MzM1.DYWJGA.MTk2shJgLEoC7LWIHot0J0Uzl1M')
