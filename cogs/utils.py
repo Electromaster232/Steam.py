8ee1cae4a320160b1b27293fc2506292a153a8ac
Adam <admin@example.com>
2018-03-13 18:42:53 +0000

    changed stuff to be async pt1

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,4 +1,4 @@
-import requests
+import aiohttp
 
 
 class Utils:
@@ -6,9 +6,9 @@ class Utils:
     def vanitytosteamid(vanityurl):
         """Convert a vanity url to SteamID64"""
 
-        response = requests.get(
-            "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=FF0EEF99E5BD63F29FC0F938A56F115C&vanityurl=" + vanityurl)
-        resjson = response.json()
+        async with aiohttp.get(
+            "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=FF0EEF99E5BD63F29FC0F938A56F115C&vanityurl=" + vanityurl) as r:
+            resjson = r.json()
         if resjson['response']['success'] != 1:
             return False
         return resjson['response']['steamid']
@@ -16,8 +16,8 @@ class Utils:
     def gametoid(gamename):
         """Convert a game name to its ID"""
 
-        response = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v2")
-        response = response.json()
+        async with aiohttp.get("http://api.steampowered.com/ISteamApps/GetAppList/v2") as r:
+            response = r.json()
         response = response['applist']['apps']
         try:
             gameid = next((item for item in response if item["name"] == gamename))
@@ -29,8 +29,8 @@ class Utils:
     def idtogame(gameid):
         """Convert game ID to game name"""
 
-        response = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v2")
-        response = response.json()
+        async with aiohttp.get("http://api.steampowered.com/ISteamApps/GetAppList/v2") as r:
+            response = r.json()
         response = response['applist']['apps']
         try:
             gamename = next((item for item in response if item["appid"] == gameid))
